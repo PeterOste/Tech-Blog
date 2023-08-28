33f41ab39ceb18f518939109d17161e220741723
unknown <peter.ostertag99@gmail.com>
2023-08-28 17:01:19 -0500

    Add postRoutes WIP

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,9 +1,66 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
 // GET / - Get existing blog posts for the homepage.
+router.get('/', (req, res) => {
+    try {
+      // Retrieve and send existing blog posts
+      // ...
+    } catch (error) {
+      // Handle error
+      // ...
+    }
+});
 
 // GET /:id - Get a specific blog post by ID.
+router.get('/:id', (req, res) => {
+    try {
+      // Retrieve and send the specific blog post with the given ID
+      // ...
+    } catch (error) {
+      // Handle error
+      // ...
+    }
+});
 
 // POST /create - Create a new blog post.
+router.post('/create', (req, res) => {
+    try {
+      // Create a new blog post using the data in req.body
+      // ...
+      // Send a success response
+      // ...
+    } catch (error) {
+      // Handle error
+      // ...
+    }
+});
 
 // PUT /:id - Update a specific blog post.
+router.put('/:id', (req, res) => {
+    try {
+      // Update the specific blog post with the given ID using the data in req.body
+      // ...
+      // Send a success response
+      // ...
+    } catch (error) {
+      // Handle error
+      // ...
+    }
+});
 
 // DELETE /:id - Delete a specific blog post.
+router.delete('/:id', (req, res) => {
+    try {
+      // Delete the specific blog post with the given ID
+      // ...
+      // Send a success response
+      // ...
+    } catch (error) {
+      // Handle error
+      // ...
+    }
+});
+
+module.exports = router;
