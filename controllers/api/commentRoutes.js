3b1be871fa493a7a611c9955c990639178a9066f
unknown <peter.ostertag99@gmail.com>
2023-08-28 16:44:06 -0500

    Add api routes index.js

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,3 @@
+// GET /:postId/comments - Get comments for a specific blog post.
+
+// POST /:postId/comments - Create a new comment for a specific blog post.
---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,9 @@
+// GET / - Get existing blog posts for the homepage.
+
+// GET /:id - Get a specific blog post by ID.
+
+// POST /create - Create a new blog post.
+
+// PUT /:id - Update a specific blog post.
+
+// DELETE /:id - Delete a specific blog post.
