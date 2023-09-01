1546758c94c108b15df5e973a250680309feb853
unknown <peter.ostertag99@gmail.com>
2023-09-01 16:30:18 -0500

    Finish commentRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,5 +3,35 @@ const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // GET /:postId/comments - Get comments for a specific blog post.
+router.get('/:postId/comments', async (req, res) => {
+    try {
+        const postId = req.params.postId;
+        const comments = await Comment.findAll({
+            where: { post_id: postId },
+        });
+
+        res.status(200).json(comments);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // POST /:postId/comments - Create a new comment for a specific blog post. 
+router.post('/:postId/comments', withAuth, async (req, res) => {
+    try {
+        const postId = req.params.postId;
+        const userId = req.session.user_id; 
+
+        const newComment = await Comment.create({
+            text: req.body.text,
+            post_id: postId,
+            user_id: userId,  
+        });
+
+        res.status(201).json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -6,6 +6,7 @@ const withAuth = require('../../utils/auth');
 router.get('/', async (req, res) => {
     try {
       const postData = await Post.findAll();
+      
       res.status(200).json(postData);
     } catch (err) {
       res.status(500).json(err);
@@ -20,6 +21,7 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: 'No post found with this id' });
       return;
     }
+
     res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
