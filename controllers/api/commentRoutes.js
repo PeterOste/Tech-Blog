2527d3021f537775646b1c690f0f5446dbf7149d
unknown <peter.ostertag99@gmail.com>
2023-09-01 13:10:25 -0500

    Add update and delete to post api

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,4 +4,4 @@ const withAuth = require('../../utils/auth');
 
 // GET /:postId/comments - Get comments for a specific blog post.
 
-// POST /:postId/comments - Create a new comment for a specific blog post.
+// POST /:postId/comments - Create a new comment for a specific blog post. 
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -25,7 +25,7 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /create - Create a new blog post.
-router.post('/create', (req, res) => {
+router.post('/create', withAuth, async (req, res) => {
     try {
       // Create a new blog post using the data in req.body
       // ...
@@ -38,28 +38,51 @@ router.post('/create', (req, res) => {
 });
 
 // PUT /:id - Update a specific blog post.
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, async (req, res) => {
     try {
-      // Update the specific blog post with the given ID using the data in req.body
-      // ...
-      // Send a success response
-      // ...
-    } catch (error) {
-      // Handle error
+      const updatedPost = await Post.update(
+        {
+          title: req.body.title,
+          content: req.body.content,
+        },
+        {
+          where: {
+            id: req.params.id,
+            user_id: req.session.user_id,
+          },
+        }
+      );
+
+      if (updatedPost[0] === 0) {
+        res.status(404).json({ message: 'No post found with this id'});
+        return;
+      }
+      
+      res.status(200).json(updatedPost);
+    } catch (err) {
+      res.status(500).json(err);
     }
 });
 
 // DELETE /:id - Delete a specific blog post.
-router.delete('/:id', (req, res) => {
-    try {
-      // Delete the specific blog post with the given ID
-      // ...
-      // Send a success response
-      // ...
-    } catch (error) {
-      // Handle error
-      // ...
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
     }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
